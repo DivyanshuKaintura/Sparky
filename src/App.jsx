4a4b76b72921eafaacf7160ec165010cc407d600
Divyanshu <divyanshukaintura789@gmail.com>
2025-06-28 21:46:27 +0530

    first commits

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Test from './pages/test';
+import Community from './pages/community';
+
+function App() {
+
+  return (
+    <div>hello
+      <Test />
+      <Community />
+    </div>
+  );
+}
+
+export default App
